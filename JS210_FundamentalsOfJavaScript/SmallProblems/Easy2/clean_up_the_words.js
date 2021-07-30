e92859821a719537c3121ddc385ed8d05f4207b0
Sven Garson <garson_sven@hotmail.com>
2021-07-30 11:28:04 +0200

    Add rest of Easy 2 solutions up to and including the last exercise

--- JS210_FundamentalsOfJavaScript/SmallProblems/Easy2/clean_up_the_words.js
+++ JS210_FundamentalsOfJavaScript/SmallProblems/Easy2/clean_up_the_words.js
@@ -1,7 +1,56 @@
+const SPACE_CHARACTER = ' ';
+
+function isModernEnglishAlphaCharacter(character) {
+  let result;
+
+  // we need to account for the the character is 'undefined' when
+  // the index used is out of bounds
+  if (!character || character.length !== 1) { 
+    result = false;
+  } else {
+    result = (character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z');
+  }
+
+  return result;
+}
+
 function cleanUp(string) {
   let cleanedUpString = '';
+
+  for (let currentCharIndex = 0; currentCharIndex < string.length; currentCharIndex += 1) {
+    const currentChar = string[currentCharIndex];
+    const nextChar = string[currentCharIndex + 1];
+    
+    const currentCharIsAlpha = isModernEnglishAlphaCharacter(currentChar);
+    const nextCharIsAlpha = isModernEnglishAlphaCharacter(nextChar);
+    const nextCharIsPastLast = !nextChar;
+
+    if (currentCharIsAlpha) {
+      cleanedUpString += currentChar;
+    } else if (nextCharIsAlpha || nextCharIsPastLast) {
+      cleanedUpString += SPACE_CHARACTER;
+    }
+  }
 
   return cleanedUpString;
 }
 
 console.log(cleanUp("---what's my +*& line?") === ' what s my line ');
+
+/*
+    # Further exploration
+
+      Implement the same solution using regular expressions.
+
+    # Approach
+
+      1. Select string portions that are not modern english alphabetical characters
+      2. Replace these portions with a single space
+
+*/
+
+function cleanUpRegex(string) {
+  return string.replaceAll(/[^(a-z)]+/gi, SPACE_CHARACTER);
+}
+
+console.log(cleanUpRegex("---what's my +*& line?") === ' what s my line ');
--- JS210_FundamentalsOfJavaScript/SmallProblems/Easy2/what_century_is_that.js
+++ JS210_FundamentalsOfJavaScript/SmallProblems/Easy2/what_century_is_that.js
@@ -1,38 +1,49 @@
-const EXCEPTIONAL_NUMBERS = [11, 12, 13];
-
 function centuryForYear(year) {
   return Math.ceil(year / 100);
 }
 
-function numberOrdinal(number) {
-  const onesDigitToOrdinal = {
+function singleDigitToOrdinal(digit) {
+  // this function asscumes that the digit is a single digit
+  const singleDigitOrdinalMap = {
     1: 'st',
     2: 'nd',
     3: 'rd'
   };
 
-  const numberOnesPlace = number % 100;
-  const numberOrdinal = onesDigitToOrdinal[String(numberOnesPlace)];
+  const digitAsString = String(digit);
+  const digitOrdinal = singleDigitOrdinalMap[digitAsString];
 
-  if (!numberOrdinal || EXCEPTIONAL_NUMBERS.includes(numberOnesPlace)) {
-    return 'th';
+  return digitOrdinal ? digitOrdinal : 'th';
+}
+
+function numberOrdinal(number) {
+  const relevantOrdinalDigits = number % 100;
+  const exceptionalNumbers = [11, 12, 13];
+
+  let ordinal;
+
+  if (exceptionalNumbers.includes(relevantOrdinalDigits)) {
+    ordinal = 'th';
   } else {
-    return numberOrdinal;
+    const lastNumberDigit = relevantOrdinalDigits % 10;
+    ordinal = singleDigitToOrdinal(lastNumberDigit);
   }
+
+  return ordinal;
 }
 
 function century(year) {
   const yearCentury = centuryForYear(year);
-  const yearCenturyOrdinal = numberOrdinal(yearCentury);
-  return String(yearCentury) + yearCenturyOrdinal;
+  const centuryOrdinal = numberOrdinal(yearCentury)
+  return `${yearCentury}${centuryOrdinal}`;
 }
 
-console.log(century(2000) === "20th");
-console.log(century(2001) === "21st");
-console.log(century(1965) === "20th");
-console.log(century(256) === "3rd");
-console.log(century(5) === "1st");
-console.log(century(10103) === "102nd");
-console.log(century(1052) === "11th");
-console.log(century(1127) === "12th");
-console.log(century(11201) === "113th");
+console.log(century(2000) === "20th")
+console.log(century(2001) === "21st")
+console.log(century(1965) === "20th")
+console.log(century(256) === "3rd")
+console.log(century(5) === "1st")
+console.log(century(10103) === "102nd")
+console.log(century(1052) === "11th")
+console.log(century(1127) === "12th")
+console.log(century(11201) === "113th")
