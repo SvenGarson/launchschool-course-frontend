035ebd79399fcbbe078662b142c4306c1e9ca387
Sven Garson <garson_sven@hotmail.com>
2021-08-07 09:53:58 +0200

    Add all solutions to JavaScript Language Fundamentals exercises 'Objects'. continue here: https://launchschool.com/lessons/79b41804/assignments/0bc5138c and finish all the other indicated exercises

--- JS210_FundamentalsOfJavaScript/JavaScriptLanguageFundamentals/Objects/after_midnight_part_2.js
+++ JS210_FundamentalsOfJavaScript/JavaScriptLanguageFundamentals/Objects/after_midnight_part_2.js
@@ -1,34 +1,47 @@
-const MINUTES_PER_HOUR = 60;
-const HOURS_PER_DAY = 24;
-const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+const MINUTES_PER_MILLISECONDS = 1 / 60000;
+
+function millisecondsToMinutes(milliseconds) {
+  return milliseconds * MINUTES_PER_MILLISECONDS;
+}
+
+function deltaDateTimeInMilliseconds(date1, date2) {
+  return Math.abs(date1.getTime() - date2.getTime());
+}
+
+function dateIsMidnight(date) {
+  return (date.getHours() === 0) && (date.getMinutes() === 0);
+}
+
+function deltaTimeInDirection(clockTime, direction) {
+  const partialBaseDate = '01 Jan 2021';
+  const midnight = new Date(`${partialBaseDate} 00:00`);
+  const nextDay = new Date(`${partialBaseDate} ${clockTime}`);
+
+  /*
+      If the given clock time string is NOT midnight and the direction is
+      negative, use yesterday set to the given clock time as the day to
+      calcaluate delta time from, otherwise use today.
+  */
+
+  if (!dateIsMidnight(nextDay) && direction < 0) {
+    nextDay.setDate(nextDay.getDate() - 1);
+  }
+
+  const deltaTimeInMilliseconds = deltaDateTimeInMilliseconds(midnight, nextDay);
+  const deltaTimeInSeconds = millisecondsToMinutes(deltaTimeInMilliseconds);
+
+  return deltaTimeInSeconds;
+}
 
 function afterMidnight(timeStr) {
-  const timeComponents = timeStr.split(':');
-  const hours = parseInt(timeComponents[0], 10);
-  const minutes = parseInt(timeComponents[1], 10);
-
-  return hours * MINUTES_PER_HOUR + minutes;
+  return deltaTimeInDirection(timeStr, 1);
 }
 
 function beforeMidnight(timeStr) {
-  let deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
-  if (deltaMinutes === MINUTES_PER_DAY) {
-    deltaMinutes = 0;
-  }
-
-  return deltaMinutes;
+  return deltaTimeInDirection(timeStr, -1);
 }
 
 console.log(afterMidnight('00:00') === 0);
 console.log(beforeMidnight('00:00') === 0);
 console.log(afterMidnight('12:34') === 754);
 console.log(beforeMidnight('12:34') === 686);
-
-/*
-
-    Something along the lines of
-
-      get today
-      get today wi
-
-*/
